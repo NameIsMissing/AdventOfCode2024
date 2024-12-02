821e8f7d7b0543a9d6b09971d830e98b437ba727
NameIsMissing <67933335+NameIsMissing@users.noreply.github.com>
2024-12-02 01:03:17 -0500

    Added comments on Day 2

--- AdventOfCode/src/Day2.java
+++ AdventOfCode/src/Day2.java
@@ -15,11 +15,19 @@ public class Day2 {
                 numValidReports++;
             }
         }
-        System.out.println(numValidReports);
+        System.out.println("Day 2 Solution: " + numValidReports);
     }
 
+    /* A report is considered valid if:
+
+        - The entire array is increasing/decreasing (no repeated values)
+        - The gap between two consecutive elements is 1 or more, but no more than 3
+            (1 <= x <= 3)
+    */
     public static boolean isValidReport(ArrayList<Integer> report) {
+        // Even if the information in the array is incorrect, the code inside of the for loops will remove invalid 'reports'
         if (report.get(0) < report.get(1)) {
+            // The array is detected as increasing
             for (int i = 1; i < report.size() - 1; i++) {
                 int beforeDifference = report.get(i) - report.get(i - 1);
                 int afterDifference = report.get(i + 1) - report.get(i);
@@ -33,6 +41,7 @@ public class Day2 {
             }
             return true;
         } else if (report.get(0) > report.get(1)) {
+            // Decreasing array
             for (int i = 1; i < report.size() - 1; i++) {
                 int beforeDifference = report.get(i) - report.get(i - 1);
                 int afterDifference = report.get(i + 1) - report.get(i);
@@ -48,20 +57,20 @@ public class Day2 {
         return false;
     }
 
+    // The following is code that takes the input from a text file and initializes it for use in finding the solution
     public static void initializeArrays() {
         try {
             File day1Input = new File("src/Inputs/Day2Input.txt");
             Scanner fileScanner = new Scanner(day1Input);
             reports = new ArrayList<ArrayList<Integer>>(1000);
-            // initialize shit
             while (fileScanner.hasNextLine()) {
                 String line = fileScanner.nextLine();
                 ArrayList<Integer> report = new ArrayList<Integer>();
-                String[] reportNums = line.split(" ");
+                String[] reportNums = line.split(" "); // Line gets converted to string array of numbers
                 for (String num : reportNums) {
-                    report.add(Integer.parseInt(num));
+                    report.add(Integer.parseInt(num)); // String array of numbers gets convered to an array list of integers
                 }
-                reports.add(report);
+                reports.add(report); // Add the list of integers to the main list of all reports
             }
         } catch (FileNotFoundException e) {
             System.out.println("File could not be opened.");
