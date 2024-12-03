9f203952b0fac20be9d31af4ce3d6d287c96cc0d
NameIsMissing <67933335+NameIsMissing@users.noreply.github.com>
2024-12-03 01:34:23 -0500

    Create Day3P2.java

---
+++ AdventOfCode/src/Day3/Day3P2.java
@@ -0,0 +1,69 @@
+package Day3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day3P2 {
+
+    private static ArrayList<String> corruptedData;
+    private static ArrayList<Integer> mulIndexes;
+
+    public static void main(String[] args) {
+        initializeArrays();
+
+        int sumOfProducts = 0;
+        for (int i = 0; i < mulIndexes.size(); i += 2) {
+            sumOfProducts += isValidMul(i);
+        }
+        System.out.println(sumOfProducts);
+    }
+
+    private static int isValidMul(int index) {
+        String line = corruptedData.get(mulIndexes.get(index + 1));
+        line = line.substring(mulIndexes.get(index));
+        line = line.substring(0, line.indexOf(")") + 1);
+        if (!line.contains("(") || !line.contains(",") || !line.contains(")")) {
+            return 0;
+        }
+        try {
+            int num1 = Integer.parseInt(line.substring(4, line.indexOf(",")));
+            int num2 = Integer.parseInt(line.substring(line.indexOf(",") + 1, line.indexOf(")")));
+            return num1 * num2;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    private static void initializeArrays() {
+        try {
+            File input = new File("src/Day3/Day3Input.txt");
+            Scanner fileScanner = new Scanner(input);
+            mulIndexes = new ArrayList<Integer>();
+            corruptedData = new ArrayList<String>();
+
+            boolean enabled = true;
+            int lineNumber = 0;
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                corruptedData.add(line);
+
+                for (int i = 0; i < line.length() - 3; i++) {
+                    if (line.startsWith("don't()", i)) {
+                        enabled = false;
+                    } else if (line.startsWith("do()", i)) {
+                        enabled = true;
+                    } else if (enabled && line.startsWith("mul", i)) {
+                        mulIndexes.add(i);
+                        mulIndexes.add(lineNumber);
+                    }
+                }
+                lineNumber++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File could not be opened.");
+            e.printStackTrace();
+        }
+    }
+}
